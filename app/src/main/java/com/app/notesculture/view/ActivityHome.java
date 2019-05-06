f3a9336e2008922c1175b68624678de594082279
Randhir <randhir286@gmail.com>
2019-05-06 11:17:18 +0530

    commit dated 6may 2019

--- app/src/main/java/com/app/notesculture/view/ActivityHome.java
+++ app/src/main/java/com/app/notesculture/view/ActivityHome.java
@@ -5,12 +5,11 @@ import android.os.Bundle;
 
 import com.app.notesculture.R;
 
-
-public class ActivityLogin extends AppCompatActivity {
+public class ActivityHome extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_home);
     }
 }
--- app/src/main/java/com/app/notesculture/view/ActivityUserProfile.java
+++ app/src/main/java/com/app/notesculture/view/ActivityUserProfile.java
@@ -3,6 +3,8 @@ package com.app.notesculture.view;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.app.notesculture.R;
+
 public class ActivityUserProfile extends AppCompatActivity {
 
     @Override
